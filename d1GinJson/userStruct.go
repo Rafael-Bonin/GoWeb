76abf7fb8fec10333c1c31bf7cb7db0965a121b0
Rafael Bonin <rafalyf11@gmail.com>
2023-06-01 17:59:43 -0300

    feat: exercicios tarde go web

--- d1GinJson/userStruct.go
+++ d1GinJson/userStruct.go
@@ -1,2 +1,45 @@
 package d1ginjson
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Id int `json:"id"`
+	Nome string `json:"nome"`
+	Sobrenome string `json:"sobrenome"`
+	Email string `json:"email"`
+	Idade int `json:"idade"`
+	Altura int `json:"altura"`
+	Ativo bool `json:"ativo"`
+	DataDeCriacao string `json:"data_de_criacao"`
+}
+
+
+func GetAll(c *gin.Context) {
+	mockedUsers := []User{{Id: 1, Nome: "Rafael", Sobrenome: "Bonin", Email: "rafalyf11@gmail.com", Idade: 21, Altura: 179, Ativo: true, DataDeCriacao: "22/05/2023"}, {Id: 2, Nome: "leafar", Sobrenome: "ninob", Email: "fake@email.com", Idade: 30, Altura: 160, Ativo: true, DataDeCriacao: "11/01/2002"}}
+	var filtered  []User
+	for _, value := range mockedUsers {
+		if value.Nome == c.Query("nome") {
+		filtered = append(filtered, value)
+		}
+	}
+
+	c.JSON(200, gin.H{ "users": filtered })
+}
+
+func GetById(c *gin.Context) {
+	mockedUsers := []User{{Id: 1, Nome: "Rafael", Sobrenome: "Bonin", Email: "rafalyf11@gmail.com", Idade: 21, Altura: 179, Ativo: true, DataDeCriacao: "22/05/2023"}, {Id: 2, Nome: "leafar", Sobrenome: "ninob", Email: "fake@email.com", Idade: 30, Altura: 160, Ativo: true, DataDeCriacao: "11/01/2002"}}
+
+	var user User
+
+	for _, value := range mockedUsers {
+		if fmt.Sprint(value.Id) == c.Param("id") {
+			user = value
+			}
+	}
+
+	c.JSON(200, gin.H{ "User": user })
+}
--- main.go
+++ main.go
@@ -1,30 +1,22 @@
 package main
 
 import (
+	d1ginjson "github.com/Rafael-Bonin/GoWeb/d1GinJson"
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	Id int `json:"id"`
-	Nome string `json:"nome"`
-	Sobrenome string `json:"sobrenome"`
-	Email string `json:"email"`
-	Idade int `json:"idade"`
-	Altura int `json:"altura"`
-	Ativo bool `json:"ativo"`
-	DataDeCriacao string `json:"data_de_criacao"`
-}
+
 
 func main() {
-	mockedUsers := []User{{Id: 1, Nome: "Rafael", Sobrenome: "Bonin", Email: "rafalyf11@gmail.com", Idade: 21, Altura: 179, Ativo: true, DataDeCriacao: "22/05/2023"}, {Id: 2, Nome: "leafar", Sobrenome: "ninob", Email: "fake@email.com", Idade: 30, Altura: 160, Ativo: true, DataDeCriacao: "11/01/2002"}}
+
 	router := gin.Default()
 
 	router.GET("ola-eu", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Ola Rafael",})
 	})
-	router.GET("users", func(c *gin.Context) {
-		c.JSON(200, gin.H{ "users": mockedUsers })
-	})
+	router.GET("users", d1ginjson.GetAll)
+
+	router.GET("users/:id", d1ginjson.GetById)
 
 	router.Run()
 }
